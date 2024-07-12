52a10e3639eed31affdc7ab53c502a6ef1666f70
hollybible <kartzew1976@mail.ru>
2024-07-12 22:01:43 +0300

    Added commands and exception

--- src/ConsoleRunner.java
+++ src/ConsoleRunner.java
@@ -1,16 +1,11 @@
 import entity.Result;
 
-import java.util.Arrays;
 
 public class ConsoleRunner {
     public static void main(String[] args) {
 
 
-      //  int key = 123; // <-- args
-
-       // String text = "Привет!"; // <-- args
-        //String result = "....."; // <-- args
-         Application application = new Application();
+        Application application = new Application();
          Result result =application.run(args);
         System.out.println(result);
     }
